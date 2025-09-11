a7a5ff1ba4ebc0c74808864460b4de3cddaf3649
Lev <for.inst.techies@mail.ru>
2025-09-11 18:35:23 +0300

    Sprites
    Sprites for character variation

--- waymark/scenes/game/main_game/main_game.gd
+++ waymark/scenes/game/main_game/main_game.gd
@@ -3,6 +3,8 @@ class_name MainGame extends Node2D
 
 func _ready() -> void:
 	Signals.start_new_turn.connect(_new_turn)
+	# TODO make game to get start_resources that player have
+	get_tree().get_first_node_in_group("character").set_money(100.0)
 	
 	GlobalUi.call_deferred("create_reources_panel")
 
